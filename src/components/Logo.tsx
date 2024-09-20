41ce71eab8ad6b7604a85e74c76607ba76ab41d8
Walter Reyes <desarrolladorweb@grupofajardo.com.ec>
2024-09-19 21:00:41 -0500

    add: maquetando layout

---
+++ src/components/Logo.tsx
@@ -0,0 +1,6 @@
+
+export const Logo = () => {
+    return (
+        <img src="/logo_devguardiancode.png" alt="DevGuardianCode Logo" />
+    )
+}
--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,11 +1,26 @@
 import { Outlet } from "react-router-dom"
+import { Logo } from "../components/Logo"
 
 export const AppLayout = () => {
     return (
         <>
-            <div>AppLayout</div>
+            <header className="bg-gray-800 py-5">
+                <div className="max-w-screen-2xl mx-auto flex flex-col lg:flex-row justify-between items-center">
+                    <div className="w-16">
+                        <Logo />
+                    </div>
+                </div>
+            </header>
 
-            <Outlet />
+            <section className="max-w-screen-2xl mx-auto mt-10 p-5">
+                <Outlet />
+            </section>
+
+            <footer className="py-5">
+                <p className="text-center">
+                    Todos los derechos reservados {new Date().getFullYear()}
+                </p>
+            </footer>
 
         </>
 
