f1fde8925a53a2f0f5c25b20bcfde4975f73a847
Walter Reyes <desarrolladorweb@grupofajardo.com.ec>
2024-09-22 19:25:23 -0500

    add: Validar respuesta de endpoint que obtiene Proyectos usando ZOD

--- src/views/DashboardView.tsx
+++ src/views/DashboardView.tsx
@@ -1,7 +1,17 @@
+import { getProjects } from "@/api/ProjectAPI"
+import { useQuery } from "@tanstack/react-query"
 import { Link } from "react-router-dom"
 
 export const DashboardView = () => {
-    return (
+
+    const { data, isLoading } = useQuery({
+        queryKey: ['projects'],
+        queryFn: getProjects
+    })
+
+    if (isLoading) return <p>Cargando...</p>
+
+    if (data) return (
         <>
             <h1 className="text-5xl font-black">Mis Proyectos</h1>
             <p className="text-2xl font-light text-gray-500 mt-5">Maneja y Administra tus Proyectos</p>
@@ -14,6 +24,13 @@ export const DashboardView = () => {
                 </Link>
             </nav>
 
+            {data.length ? (
+                <p>Si hay Proyectos</p>
+            ) : (
+                <p className="text-center py-20">
+                    Aún no tienes proyectos, <Link to="/projects/create" className="text-pink-600">Crea tu primer proyecto</Link>
+                </p>
+            )}
         </>
     )
 }
