ad30915327a4f366e9da8872369cb20d7c886191
Walter Reyes <desarrolladorweb@grupofajardo.com.ec>
2024-09-23 17:51:38 -0500

    add: agregar funcionalidad para eliminar proyecto y refrescar listado de proyectos con useQueryClient-invalidateQueries

--- src/views/DashboardView.tsx
+++ src/views/DashboardView.tsx
@@ -1,15 +1,28 @@
 import { Fragment } from 'react'
 import { Menu, MenuButton, MenuItem, MenuItems, Transition } from '@headlessui/react'
 import { EllipsisVerticalIcon } from '@heroicons/react/20/solid'
-import { getProjects } from "@/api/ProjectAPI"
-import { useQuery } from "@tanstack/react-query"
+import { deleteProject, getProjects } from "@/api/ProjectAPI"
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query"
 import { Link } from "react-router-dom"
+import { toast } from 'react-toastify'
 
 export const DashboardView = () => {
 
     const { data, isLoading } = useQuery({
         queryKey: ['projects'],
         queryFn: getProjects
+    })
+
+    const queryClient = useQueryClient()
+    const { mutate } = useMutation({
+        mutationFn: deleteProject,
+        onError: (error) => {
+            toast.error(error.message)
+        },
+        onSuccess: (data) => {
+            toast.success(data)
+            queryClient.invalidateQueries({ queryKey: ['projects'] })
+        }
     })
 
     if (isLoading) return <p>Cargando...</p>
@@ -73,7 +86,7 @@ export const DashboardView = () => {
                                                 <button
                                                     type='button'
                                                     className='block px-3 py-1 text-sm leading-6 text-red-500'
-                                                    onClick={() => { }}
+                                                    onClick={() => mutate(project._id)}
                                                 >
                                                     Eliminar Proyecto
                                                 </button>
