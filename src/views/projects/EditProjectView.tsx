4ef4823fa2bc3d22711cc3f71b2cf86159dac68b
Walter Reyes <desarrolladorweb@grupofajardo.com.ec>
2024-09-23 13:44:39 -0500

    add: Obtener proyecto a editar con el param obtenido

--- src/views/projects/EditProjectView.tsx
+++ src/views/projects/EditProjectView.tsx
@@ -1,9 +1,17 @@
+import { getProjectById } from "@/api/ProjectAPI"
+import { useQuery } from "@tanstack/react-query"
 import { useParams } from "react-router-dom"
 
 export const EditProjectView = () => {
 
     const params = useParams()
     const projectId = params.projectId!
+
+    const { data, isLoading, error, isError } = useQuery({
+        queryKey: ['editProject', projectId],
+        queryFn: () => getProjectById(projectId),
+        //retry: false
+    })
 
     return (
         <div>EditProjectView: {projectId}</div>
