8c3e2dce9f818119666d26fc3fb3019da549557c
Walter Reyes <desarrolladorweb@grupofajardo.com.ec>
2024-09-26 22:27:47 -0500

    add: agregar funcionalidad de listar tareas por status

--- src/components/tasks/AddTaskModal.tsx
+++ src/components/tasks/AddTaskModal.tsx
@@ -4,7 +4,7 @@ import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import { TaskFormData } from '@/types';
 import { useForm } from 'react-hook-form';
 import TaskForm from './TaskForm';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { createTask } from '@/api/TaskAPI';
 import { toast } from 'react-toastify';
 
@@ -28,12 +28,14 @@ export default function AddTaskModal() {
     }
     const { register, handleSubmit, reset, formState: { errors } } = useForm({ defaultValues: initialValues })
 
+    const queryClient = useQueryClient()
     const { mutate } = useMutation({
         mutationFn: createTask,
         onError: (error: Error) => {
             toast.error(error.message)
         },
         onSuccess: (data) => {
+            queryClient.invalidateQueries({ queryKey: ['editProject', projectId] })
             toast.success(data)
             reset()
             navigate(location.pathname, { replace: true })
