ea47d803fcd3453e58756a2130f81dc875a211d3
Walter Reyes <desarrolladorweb@grupofajardo.com.ec>
2024-09-30 22:32:04 -0500

    add: mostrar modal con datos de la tarea, validación con zod y formato de fecha createdAt y updatedAt

--- src/api/TaskAPI.ts
+++ src/api/TaskAPI.ts
@@ -1,5 +1,5 @@
 import api from "@/lib/axios"
-import { Project, Task, TaskFormData } from "@/types"
+import { Project, Task, TaskFormData, taskSchema } from "@/types"
 import { isAxiosError } from "axios"
 
 type TaskAPI = {
@@ -24,7 +24,10 @@ export async function getTaskById({ projectId, taskId }: Pick<TaskAPI, 'projectI
     try {
         const url = `/projects/${projectId}/tasks/${taskId}`
         const { data } = await api(url)
-        return data
+        const response = taskSchema.safeParse(data)
+        if (response.success) {
+            return response.data
+        }
     } catch (error) {
         if (isAxiosError(error) && error.response) {
             throw new Error(error.response.data.error)
