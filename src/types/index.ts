5d6a3c17d772ed48c98c64006c21974813da3417
Walter Reyes <desarrolladorweb@grupofajardo.com.ec>
2024-10-06 19:53:45 -0500

    add: crear funcionalidad para registrar usuario(Zod, Routing, RegisterView)

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,17 @@
 import { create } from 'domain'
 import { z } from 'zod'
 
+//Auth & Users
+const authSchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+    password_confirmation: z.string().min(6)
+})
+
+type Auth = z.infer<typeof authSchema>
+export type UserLoginForm = Pick<Auth, 'email' | 'password'>
+export type UserRegistrationForm = Pick<Auth, 'name' | 'email' | 'password' | 'password_confirmation'>
 // Task
 export const taskStatusSchema = z.enum(["pending", "onHold", "inProgress", "underReview", "completed"])
 export type TaskStatus = z.infer<typeof taskStatusSchema>
