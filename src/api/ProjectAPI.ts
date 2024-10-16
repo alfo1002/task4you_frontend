1704159d24806996671bce28dd3c4138466ff092
Walter Reyes <desarrolladorweb@grupofajardo.com.ec>
2024-10-15 21:32:53 -0500

    add: incorporar interceptors para no enviar el token en cada petición manualmente

--- src/api/ProjectAPI.ts
+++ src/api/ProjectAPI.ts
@@ -14,14 +14,8 @@ export async function createProject(formData: ProjectFormData) {
 }
 
 export async function getProjects() {
-    const token = localStorage.getItem('auth_token')
-    console.log("token: ", token)
     try {
-        const { data } = await api('/projects', {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        })
+        const { data } = await api('/projects')
         const response = dashboardProjectSchema.safeParse(data)
         if (response.success) {
             return response.data
