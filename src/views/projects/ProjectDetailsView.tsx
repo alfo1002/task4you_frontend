6e05884f83dcfa2014b7d7eeefbc2d39285866bf
Walter Reyes <desarrolladorweb@grupofajardo.com.ec>
2024-10-23 22:10:58 -0500

    add: agregar validación si es manager para botones de crear o añadir tareas

--- src/views/projects/ProjectDetailsView.tsx
+++ src/views/projects/ProjectDetailsView.tsx
@@ -3,11 +3,14 @@ import AddTaskModal from "@/components/tasks/AddTaskModal"
 import { EditTaskData } from "@/components/tasks/EditTaskData"
 import { TaskList } from "@/components/tasks/TaskList"
 import TaskModalDetails from "@/components/tasks/TaskModalDetails"
+import { useAuth } from "@/hooks/useAuth"
+import { isManager } from "@/utils/policies"
 import { useQuery } from "@tanstack/react-query"
 import { Link, Navigate, useNavigate, useParams } from "react-router-dom"
 
 export const ProjectDetailsView = () => {
 
+    const { data: user, isLoading: authLoading } = useAuth()
     const navigate = useNavigate()
     const params = useParams()
     const projectId = params.projectId!
@@ -16,29 +19,33 @@ export const ProjectDetailsView = () => {
         queryFn: () => getProjectById(projectId),
         retry: false
     })
-    if (isLoading) return <p>Cargando...</p>
+    if (isLoading && authLoading) return <p>Cargando...</p>
     if (isError) return <Navigate to="/404" />
-    if (data) return (
+    if (data && user) return (
         <>
             <h1 className="text-5xl font-black">{data.projectName}</h1>
             <p className="text-2xl font-light text-gray-500 mt-5">{data.description}</p>
 
-            <nav className="my-5 flex gap-3">
-                <button
-                    type="button"
-                    className="bg-pink-500 hover:bg-pink-700 px-10 py-3 text-white text-xl
+            {isManager(data.manager, user._id) && (
+                <nav className="my-5 flex gap-3">
+                    <button
+                        type="button"
+                        className="bg-pink-500 hover:bg-pink-700 px-10 py-3 text-white text-xl
                     font-bold cursor-pointer transition-colors duration-300 ease-in-out"
-                    onClick={() => navigate(location.pathname + '?newTask=true')}
-                >
-                    Agregar Tarea
-                </button>
-                <Link
-                    className="bg-pink-700 hover:bg-pink-900 px-10 py-3 text-white text-xl
+                        onClick={() => navigate(location.pathname + '?newTask=true')}
+                    >
+                        Agregar Tarea
+                    </button>
+                    <Link
+                        className="bg-pink-700 hover:bg-pink-900 px-10 py-3 text-white text-xl
                 font-bold cursor-pointer transition-colors duration-300 ease-in-out"
-                    to={'team'}>
-                    Colaboradores
-                </Link>
-            </nav>
+                        to={'team'}>
+                        Colaboradores
+                    </Link>
+                </nav>
+            )}
+
+
             <TaskList
                 tasks={data.tasks}
             />
